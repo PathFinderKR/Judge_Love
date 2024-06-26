87625a8d7295a87b939b45c10a8e0ab3d1f52cb4
PathFinderKR <cheiron1103@gmail.com>
2024-06-26 10:56:35 +0900

    Judge Love

--- manage.py
+++ manage.py
@@ -17,7 +17,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-pip
 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Judge_Love.settings')
